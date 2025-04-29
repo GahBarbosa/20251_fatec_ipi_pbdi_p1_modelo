44dfbb8c1030f1d4554ee97513f3eb8134f722ac
gahbarbosa <gahbarbosa7@gmail.com>
2025-04-29 10:49:02 -0300

    feat(p1): cria base e importa dados

--- script.sql
+++ script.sql
@@ -2,7 +2,15 @@
 -- 1 Base de dados e criação de tabela
 --escreva a sua solução aqui
 
-
+CREATE TABLE STUDENTS(
+ID SERIAL PRIMARY KEY, 
+MOTHER_EDU INT,
+FATHER_EDU INT,
+SALARY INT,
+GRADE INT,
+PREP_STUDY INT,
+PREP_EXAM INT
+);
 -- ----------------------------------------------------------------
 -- 2 Resultado em função da formação dos pais
 --escreva a sua solução aqui
@@ -16,7 +24,6 @@
 -- ----------------------------------------------------------------
 -- 4 Salário versus estudos
 --escreva a sua solução aqui
-
 
 -- ----------------------------------------------------------------
 -- 5. Limpeza de valores NULL
